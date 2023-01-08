b4c4bab911a9275b1fc4d1696a215afad50f7c52
nkhlg <114756264+nkhlg@users.noreply.github.com>
2023-01-08 18:18:34 +0530

    fourth commit

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -1,6 +1,6 @@
 const ResponseModel = require('../utilities/responseModel');
-
-
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("22805011057-sfgcohpdbgp7a0gaq229o0coqlcofrhq.apps.googleusercontent.com");
 module.exports = (req, res, next) => {
     if(req.url=='/'){
         return next();
@@ -13,6 +13,26 @@ module.exports = (req, res, next) => {
         return res.status(401)
             .json(new ResponseModel(null, null, ['Unauthorized.']));
     }
+    if(token){
+        async function verify() {
+            const ticket = await client.verifyIdToken({
+                idToken: token,
+                audience: "22805011057-sfgcohpdbgp7a0gaq229o0coqlcofrhq.apps.googleusercontent.com", 
+          
+            });
+            const payload = ticket.getPayload();
+            console.log(payload)
+           
+           
+          }
+          try{
+            verify()
+            return next()
+          }
+          catch(e){
+            return res.status(401).json(new ResponseModel(null, null, ['Unauthorized.']))
+          }
+        }
 
    
 }
